26dd6b8beaf8b2c7fdaf94473b6fb5913467e46d
David Teles <davidteles@David-Teles.lan>
2018-03-27 13:50:52 +0100

    Have fun!
    Don’t knows if it works yet!

--- project/serial/ler.c
+++ project/serial/ler.c
@@ -47,7 +47,7 @@ int main(int argc, char **argv){
 		printf("\n");
 	}
     
-    bactrack(matrix, &attempt, &backtracks, size);
+    bactrack(matrix, &attempt, &backtracks, size*size);
 	
     for (i = 0; i < size * size; i++){
         for(j = 0; j < size * size; j++) {
@@ -66,9 +66,10 @@ void bactrack(int **puzzle, int *attempt, int *backtracks, int size)
     
     int i, j, k, found=0, temp=0;
     
+
+    
     int **stable = (int **)malloc(size * size * sizeof(int *));
     for(i = 0; i < size * size; i++) stable[i] = (int *)malloc(size * size * sizeof(int));
-    
     
     
     for(i=0;i<size;i++)
@@ -81,14 +82,17 @@ void bactrack(int **puzzle, int *attempt, int *backtracks, int size)
                 stable[i][j]=1;
         }
     }
-    
+    //printf("1\n");
     
     for(i=0;i<size;i++)
     {
+        //printf("2\n");
         for(j=0;j<size;j++)
         {
+            //printf("3\n");
             for(k=0;k<size;k++)
             {
+                //printf("4\n");
                 if(checkValid(puzzle, i, j, k+1, size)==1)
                 {
                     puzzle[i][j]=k+1;
@@ -100,37 +104,52 @@ void bactrack(int **puzzle, int *attempt, int *backtracks, int size)
             
             while(puzzle[i][j]==0)
             {
+                //printf("5\n");
                 found=0;
                 temp=j-1;
-                for(j=temp;j>=0;j--)
-                {
-                    if(stable[i][j]==0)
+                //printf("temp1=%d\n",temp);
+                if(temp!=-1){
+                    for(j=temp;j>=0;j--)
                     {
-                        found=1;
-                        break;
+                        //printf("6\n");
+                        if(stable[i][j]==0)
+                        {
+                            found=1;
+                            break;
+                        }
                     }
                 }
                 temp=i-1;
+                
                 if(found==0)
                 {
-                    for(i=temp;i>=0;i--)
-                    {
-                        for(j=8;j>=0;j--)
+                    //printf("temp2=%d\n",temp);
+                    if(temp!=-1){
+                        for(i=temp;i>=0;i--)
                         {
-                            if(stable[i][j]==0)
+                            //printf("7\n");
+                            
+                            for(j=size-1;j>=0;j--)
                             {
-                                found=1;
-                                break;
+                                //printf("8\n");
+                                if(stable[i][j]==0)
+                                {
+                                    found=1;
+                                    break;
+                                }
                             }
+                            if(found==1)
+                                break;
                         }
-                        if(found==1)
-                            break;
                     }
                 }
+                
                 found=0;
                 temp=puzzle[i][j]+1;
+                
                 for(k=temp;k<size;k++)
                 {
+                    //printf("9\n");
                     if(checkValid(puzzle, i, j, k+1,size)==1)
                     {
                         found=1;
