b63203becb5f3029b4ce272a1ade84f44c14eb3b
David Teles <davidteles@David-Teles.lan>
2018-03-27 12:38:39 +0100

    Backtrack implemented
    Have fun testing it

--- project/serial/ler.c
+++ project/serial/ler.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <errno.h>
 #include <omp.h>
+#include "verify.h"
 
+
+bool checkValid(int **puzzle,int i,int j,int value, int size);
 
 int main(int argc, char **argv){
     int size,i,j;
@@ -44,4 +47,104 @@ int main(int argc, char **argv){
 		printf("\n");
 	}
 	
+}
+
+
+
+void bactrack(int **puzzle, int *attempt, int *backtracks, int size)
+{
+    
+    
+    int i, j, k, found=0, temp=0;
+    
+    int **stable = (int **)malloc(size * size * sizeof(int *));
+    for(i = 0; i < size * size; i++) stable[i] = (int *)malloc(size * size * sizeof(int));
+    
+    
+    
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<size;j++)
+        {
+            if(puzzle[i][j]==0)
+                stable[i][j]=0;
+            else
+                stable[i][j]=1;
+        }
+    }
+    
+    
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<size;j++)
+        {
+            for(k=0;k<size;k++)
+            {
+                if(checkValid(puzzle, i, j, k+1, size)==1)
+                {
+                    puzzle[i][j]=k+1;
+                    break;
+                }
+                if(k==size-1)
+                    break;
+            }
+            
+            while(puzzle[i][j]==0)
+            {
+                found=0;
+                temp=j-1;
+                for(j=temp;j>=0;j--)
+                {
+                    if(stable[i][j]==0)
+                    {
+                        found=1;
+                        break;
+                    }
+                }
+                temp=i-1;
+                if(found==0)
+                {
+                    for(i=temp;i>=0;i--)
+                    {
+                        for(j=8;j>=0;j--)
+                        {
+                            if(stable[i][j]==0)
+                            {
+                                found=1;
+                                break;
+                            }
+                        }
+                        if(found==1)
+                            break;
+                    }
+                }
+                found=0;
+                temp=puzzle[i][j]+1;
+                for(k=temp;k<size;k++)
+                {
+                    if(checkValid(puzzle, i, j, k+1)==1)
+                    {
+                        found=1;
+                        puzzle[i][j]=k+1;
+                        break;
+                    }
+                }
+                if(found==0)
+                    puzzle[i][j]=0;
+            }
+        }
+    }
+}
+
+bool checkValid(int **puzzle,int i,int j,int value,int size){
+    bool a,b,c;
+    a = row_valid(puzzle,i,value,size);
+    b = collum_valid(puzzle,j,value,size);)
+    c = sq_valid(puzzle, i, j,value, size );
+    if(a && b && c){
+        return true;
+    }
+    
+    
+    return false;
 }
---
+++ project/serial/verify.h
@@ -0,0 +1,17 @@
+//
+//  verify.h
+//  
+//
+//  Created by David Teles on 27/03/18.
+//
+
+#ifndef verify_h
+#define verify_h
+
+
+#endif /* verify_h */
+bool row_valid(int** total,int row, int num,int tam);
+bool collum_valid(int** total,int collum, int num,int tam);
+bool sq_valid(int** total, int row, int collum,int num, int tam );
+bool is_valid(int** total, int row, int collum,int num, int tam );
+void print(); 
